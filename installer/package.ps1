f3aedf2bcd985bde57492bdde6763561f8d1d200
FURUHASHI Sadayuki <frsyuki@users.sourceforge.jp>
2014-05-30 14:06:09 -0700

    added installer/package.ps1

---
+++ installer/package.ps1
@@ -0,0 +1,16 @@
+Param(
+[string]$configPath
+)
+$scriptPath = (Split-Path $MyInvocation.MyCommand.Path)
+$configInfo = & "$scriptPath\..\winbuild\configuration.ps1" "$configPath"
+$VERSION = $configInfo.Configuration.version
+
+$packageName = "prestogres-odbc-$VERSION"
+
+Remove-Item "$packageName" -Recurse
+New-Item "$packageName" -itemType Directory
+Copy-Item "prestogres-odbc.msi" "$packageName\prestogres-odbc.msi"
+Copy-Item "..\readme.txt" "$packageName\README.txt"
+
+Write-Zip -Path "$packageName" -OutputPath "${packageName}.zip" -IncludeEmptyDirectories
+
